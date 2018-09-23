38fdd29e36dce738e2c69ad22d762b7b8cfb9232
joel2035 <joel.pokam@hetic.net>
2018-09-23 20:18:45 +0200

    update filter

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var app = angular.module("myApp", []);
-app.controller('appController', function($scope, $http, $location, ) {
+
+app.controller('appController', function($scope, $http, $location, $filter, ) {
   $http({
+
     method: 'GET',
     url: 'http://www-uat.tictactrip.eu/api/cities/popular/5'
 
   }).then(function successCallback(response) {
     $scope.dossiers = response.data;
+    // ajouter la saisir les mots saisir dans l'input à l'url
     var citieKey = "q";
     $scope.$watch(function() {
       return $location.search();
@@ -18,12 +21,40 @@ app.controller('appController', function($scope, $http, $location, ) {
     });
     $scope.champTri = "unique_name";
     $scope.dossierCourant = null;
+    // pour vider le champ de recherche au moment du click
+
+    $scope.razRecherche = function() {
+      $scope.citie = null;
+    };
+
+    // filtré les pays
+
+    $http({
+      method: 'GET',
+      url: 'http://www-uat.tictactrip.eu/api/cities/autocomplete/?q=P'
+    }).then(function successCallback(response) {
+      $scope.values = response.data;
+      // ajouter la saisir les mots saisir dans l'input à l'url
+      var ville = angular.merge($scope.dossiers, $scope.values);
+
+
+      // $scope.citie = "";
+      // $scope.filterDossier = function() {
+      //   return $scope.dossiers.filter(function(item) {
+      //     return (item.unique_name.toString().indexOf($scope.citie) > -1)
+      //   });
+      // };
+
+    }, function errorCallback(response) {
+      alert('essayer une autre méthode celle ci ne marche pas');
+
+    });
+
 
   }, function errorCallback(response) {
     alert('essayer une autre méthode celle ci ne marche pas');
 
   });
-
 
 
 });
